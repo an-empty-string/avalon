4313a0835a9a35e436d1165a4827675e805ceb0d
ovkulkarni <2019okulkarn@tjhsst.edu>
2016-02-28 16:46:23 -0500

    restart via bot command

---
+++ bot.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+from pyrcb import IRCBot
+from socketIO_client import SocketIO, BaseNamespace
+import threading
+import sys
+import yaml
+import subprocess as sp
+import os
+
+state = sp.Popen(['python','state.py'])
+
+conf_file = sys.argv[1] if len(sys.argv) > 1 else "config.yml"
+with open(conf_file) as f:
+    config = yaml.load(f)
+
+admins = ["fwilson", "sdrodge", "okulkarni"]
+denied = {}
+
+def plural(n, thing, interptext="", plural_form=None):
+    if not interptext:
+        interptext = " "
+    else:
+        interptext = " " + interptext + " "
+    if n == 1:
+        return "1{}{}".format(interptext, thing)
+    if not plural_form:
+        return "{}{}{}s".format(n, interptext, thing)
+    return "{}{}{}".format(n, interptext, plural_form)
+
+def lf(l):
+    if len(l) == 1:
+        return l[0]
+    if len(l) == 2:
+        return "{} and {}".format(*l)
+    return "{}, and {}".format(", ".join(l[:-1]), l[-1])
+
+class AvalonBot(IRCBot):
+
+    def on_message(self, message, nickname, channel, is_query):
+        if message.startswith("!"):
+            if nickname in denied:
+                for i in denied[nickname]:
+                    if i in message:
+                        bot.send(channel, "{}: lol nope".format(nickname))
+                        return
+        if not is_query:
+            if message.startswith("!propose ") or message.startswith("!pick "):
+                private_ns.emit('propose players', [nickname, message.split()[1:]])
+            elif message.startswith("!kill "):
+                private_ns.emit('kill player request', [nickname, message.split()[1]])
+            elif message == "!join":
+                private_ns.emit('join game request', nickname)
+            elif message.startswith("!fjoin ") and nickname in admins:
+                private_ns.emit('join game request', message.split()[1])
+            elif message == "!leave":
+                private_ns.emit("leave game request", nickname)
+            elif message.startswith("!kick ") and nickname in admins:
+                private_ns.emit("leave game request", message.split()[1])
+            elif message == "!start":
+                private_ns.emit('game start request', nickname)
+            elif message == "!help":
+                bot.send(channel, "Rules are at https://github.com/fwilson42/avalon/blob/master/rules.md | During a game: \x02!propose\x02 a quest team, \x02!kill\x02 a player as assassin, vote \x02yes\x02 or \x02no\x02 on a proposed quest team in PM, \x02pass\x02 or \x02fail\x02 a quest. | Before a game: \x02!join\x02 the game or \x02!leave\x02 it. \x02!start\x02 once you have enough players.")
+            elif message.startswith("!deny ") and nickname in admins:
+                args = message.split()
+                if args[1] not in denied:
+                    denied[args[1]] = []
+                denied[args[1]].append(args[2])
+                bot.send(channel, "{} is now denied {}".format(args[1], lf(denied[args[1]])))
+            elif message.startswith("!allowall ") and nickname in admins:
+                args = message.split()
+                denied[args[1]] = []
+                bot.send(channel, "ok!")
+            elif message == '!restart':
+                sp.Popen.terminate(state)
+                os.execl(sys.executable, sys.executable, *sys.argv)
+        else:
+            message = message.lower()
+            if message == "yes":
+                private_ns.emit('vote request', [nickname, True])
+            elif message == "no":
+                private_ns.emit('vote request', [nickname, False])
+            elif message == "pass":
+                private_ns.emit('qvote request', [nickname, True])
+            elif message == "fail":
+                private_ns.emit('qvote request', [nickname, False])
+
+
+class PublicNamespace(BaseNamespace):
+
+    def on_game_start_error(self, message):
+        bot.send(channel, message)
+
+    def on_game_start(self, args):
+        bot.send(channel, "The game is starting.")
+
+    def on_join_game_error(self, message):
+        bot.send(channel, message)
+
+    def on_leave_game_error(self, message):
+        bot.send(channel, message)
+
+    def on_join_game(self, args):
+        player, players = args
+        bot.send(channel, "Welcome to the game, {}! {}.".format(player, plural(len(players), "player")))
+
+    def on_leave_game(self, args):
+        player, players = args
+        bot.send(channel, "Bye, {}! {} left.".format(player, plural(len(players), "player")))
+
+    def on_numeric_history(self, args):
+        successes, failures = args
+        bot.send(channel, "There have been {} and {}.".format(plural(successes, "quest", "successful"), plural(failures, "quest", "failed")))
+
+    def on_history(self, history):
+        for questers, status, votes, proposed in history:
+            bot.send(channel, "History: Quest with {}: {}ed with {} to fail. Proposed by {}.".format(lf(questers), status, plural(votes, "vote"), proposed))
+
+    def on_proposal_request(self, args):
+        quest_leader, quest_size, players_text = args
+        bot.send(channel, "The quest leader is {0}. {0}, please propose a quest with {1} players by using !propose {2}".format(quest_leader, quest_size, players_text))
+
+    def on_proposal_error(self, message):
+        bot.send(channel, message)
+
+    def on_proposed_team(self, args):
+        player, players = args
+        bot.send(channel, "The proposed quest team is: {}. Vote by /msg'ing me 'yes' or 'no'.".format(lf(players)))
+
+    def on_vote_confirmation(self, player):
+        bot.send(player, "Okay, your vote has been recorded.")
+        bot.send(channel, "{} has voted.".format(player))
+
+    def on_vote_finish(self, args):
+        success, yes_votes, no_votes = args
+        bot.send(channel, "The votes are in! The vote was{} successful.".format("" if success else " not"))
+        if yes_votes:
+            bot.send(channel, "Yes votes: {}".format(lf(yes_votes)))
+        if no_votes:
+            bot.send(channel, "No votes: {}".format(lf(no_votes)))
+
+    def on_qvote_placed(self, player):
+        bot.send(channel, "{} has completed the quest.".format(player))
+
+    def on_qvote_finish(self, args):
+        res, fail_votes = args
+        bot.send(channel, "The votes are in! The quest {}ed with {} to fail.".format(res, plural(fail_votes, "vote")))
+
+    def on_game_over(self, result):
+        bot.send(channel, result)
+
+    def on_pick_target(self, player):
+        bot.send(channel, "{}, please choose a target with !kill player.".format(player))
+
+    def on_target_error(self, message):
+        bot.send(channel, message)
+
+    def on_game_over(self, message):
+        bot.send(channel, message)
+
+    def on_current_quest_failed_votes(self, message):
+        bot.send(channel, "{} have failed for this quest.".format(plural(message, "proposal")))
+
+
+class PrivateNamespace(BaseNamespace):
+
+    def on_player_role(self, args):
+        player, role_text = args
+        bot.send(player, role_text)
+
+    def on_vote_error(self, args):
+        player, message = args
+        bot.send(player, message)
+
+    def on_qvote_poke(self, args):
+        player, role = args
+        bot.send(player, "Please vote with 'pass' or '{}'.".format("pass" if role == "good" else "fail"))
+
+    def on_qvote_error(self, args):
+        player, message = args
+        bot.send(player, message)
+
+    def on_qvote_confirmation(self, args):
+        player, vote = args
+        bot.send(player, "You voted to {} the quest.".format(vote))
+
+# FIXME: kill globals
+
+channel = config["irc"]["channel"]
+sio = SocketIO(config["state"]["host"], config["state"]["port"], headers={"Authentication":config["secret"]})
+
+bot = AvalonBot()
+public_ns = sio.define(PublicNamespace, '/public')
+private_ns = sio.define(PrivateNamespace, '/private')
+
+
+def main():
+    sio_t = threading.Thread(target=sio.wait)
+    sio_t.start()
+
+    bot.connect(config["irc"]["server"], config["irc"]["port"])
+    bot.register(config["irc"]["nick"])
+    bot.join(channel)
+
+    bot.listen()
+
+if __name__ == '__main__':
+    main()
---
+++ state.py
@@ -0,0 +1,360 @@
+#!/usr/bin/env python
+import math
+import copy
+import random
+import yaml
+import functools
+from flask_socketio import SocketIO, emit, disconnect
+from flask import Flask, render_template, abort, request
+
+app = Flask(__name__)
+with open("config.yml") as f:
+    config = yaml.load(f)
+    app.config['SECRET_KEY'] = config["secret"]
+    hostname = config["state"]["host"]
+    portnum = config["state"]["port"]
+
+sio = SocketIO(app)
+game = None
+joined_players = []
+
+bad_roles = ["assassin", "mordred", "morgana", "badguy"]
+good_roles = ["merlin", "percival", "goodguy", "goodguy", "goodguy", "goodguy"]
+
+quests = {
+    5:  [2, 3, 2, 3, 3],
+    6:  [2, 3, 4, 3, 4],
+    7:  [2, 3, 3, 4, 4],
+    8:  [3, 4, 4, 5, 5],
+    9:  [3, 4, 4, 5, 5],
+    10: [3, 4, 4, 5, 5]
+}
+
+def dict_get_multi(d, keys):
+    result = []
+    for i in keys:
+        if i in d:
+            result.append(d[i])
+    return result
+
+
+def get_roles(n_players):
+    n_bad = math.ceil(n_players / 3)
+    n_good = n_players - n_bad
+    return bad_roles[:n_bad] + good_roles[:n_good]
+
+
+class AvalonGame:
+
+    def __init__(self, players):
+        self.players = copy.copy(players)
+        self.roles = {}
+        self.player_roles = {}
+        self.teams = {"bad": [], "good": []}
+        self.role_texts = {}
+        self.state = "no_game"
+        self.quest = -1
+        self.current_quest_leader = 0
+        self.questers = []
+        self.questers_voted = []
+        self.history = []
+        self.votes = {}
+        self.quest_fail_votes = 0
+        self.successes = 0
+        self.failures = 0
+        self.cqfv = 0
+
+    def get_role_text(self, player):
+        role = self.player_roles[player]
+        team = "good" if role in good_roles else "bad"
+        article = "a" if role.endswith("guy") else "the"
+        yourrole_text = "You are {} {} ({}).".format(article, role.title(), team)
+        extra_text = "You have no special information."
+        if role in ["assassin", "mordred", "morgana"]:
+            extra_text = "The bad team is: {}.".format(", ".join(self.teams["bad"]))
+        elif role == "merlin":
+            badteam = self.teams["bad"]
+            badteam.remove(self.roles["mordred"])
+            extra_text = "The bad team contains: {}.".format(", ".join(badteam))
+        elif role == "percival":
+            if "morgana" in self.roles:
+                to_send = [self.roles["merlin"], self.roles["morgana"]]
+                random.shuffle(to_send)
+                extra_text = "The Merlin and Morgana are: {}, {}.".format(*to_send)
+            else:
+                extra_text = "The Merlin is: {}.".format(self.roles["merlin"])
+
+        return "{} {}".format(yourrole_text, extra_text)
+
+    def assign_roles(self):
+        self.history.clear()
+        random.shuffle(self.players)
+        roles = get_roles(len(self.players))
+        for idx, player in enumerate(self.players):
+            self.roles[roles[idx]] = player
+            self.player_roles[player] = roles[idx]
+            self.teams["bad" if roles[idx] in bad_roles else "good"].append(player)
+
+        for player in self.players:
+            self.role_texts[player] = self.get_role_text(player)
+            sio.emit('player role', [player, self.role_texts[player]], namespace='/private')
+
+        self.current_quest_leader = random.randint(0, len(self.players))
+
+    def quest_size(self):
+        return quests[len(self.players)][self.quest]
+
+    def next_quest_leader(self):
+        self.current_quest_leader += 1
+        self.current_quest_leader %= len(self.players)
+
+        quest_size = quests[len(self.players)][self.quest]
+        quest_leader = self.players[self.current_quest_leader]
+        players_text = " ".join(["player{}".format(i + 1) for i in range(quest_size)])
+
+        sio.emit('numeric history', [self.successes, self.failures], namespace='/public')
+        sio.emit('history', self.history, namespace='/public')
+        sio.emit('proposal request', [quest_leader, quest_size, players_text], namespace='/public')
+
+        self.state = "propose_who"
+        return self.players[self.current_quest_leader]
+
+    def next_quest(self):
+        self.quest += 1
+        self.next_quest_leader()
+
+    def propose(self, player, players):
+        if self.state != "propose_who":
+            return
+        players = list(set(players))
+
+        if len(players) != quests[len(self.players)][self.quest]:
+            sio.emit('proposal error', "SIKE, THAT'S THE WRONG NUMBER", namespace='/public')
+        elif player != self.players[self.current_quest_leader]:
+            sio.emit('proposal error', "{}: You're not the quest leader. Maybe next time.".format(player), namespace='/public')
+        else:
+            for player in players:
+                if player not in self.players:
+                    sio.emit('proposal error', "{} isn't playing.".format(player), namespace='/public')
+                    return False
+
+            self.questers = players
+            sio.emit('proposed team', [self.players[self.current_quest_leader], players], namespace='/public')
+            self.votes.clear()
+            self.state = "vote_who"
+
+    def tally_votes(self):
+        return [i[0] for i in self.votes.items() if i[1]], [i[0] for i in self.votes.items() if not i[1]]
+
+    def vote_on_questers(self, player, affirmative):
+        if self.state != "vote_who":
+            return
+
+        if player not in self.players:
+            sio.emit('vote error', [player, "You can't vote if you aren't in the game."], namespace='/private')
+        else:
+            self.votes[player] = affirmative
+            sio.emit('vote confirmation', player, namespace='/public')
+
+            if len(self.votes) == len(self.players):
+                yes_votes, no_votes = self.tally_votes()
+                success = len(yes_votes) > len(no_votes)
+                sio.emit('vote finish', [success, yes_votes, no_votes], namespace='/public')
+                if success:
+                    self.state = "quester_vote"
+                    self.cqfv = 0
+                    self.do_quest()
+                else:
+                    self.cqfv += 1
+                    if self.cqfv >= 5:
+                        self.do_evil_win()
+                    else:
+                        sio.emit('current quest failed votes', self.cqfv, namespace='/public')
+                        self.next_quest_leader()
+
+    def do_quest(self):
+        self.quest_fail_votes = 0
+        self.questers_voted = []
+        for player in self.questers:
+            if player in self.teams["good"]:
+                sio.emit('qvote poke', [player, 'good'], namespace='/private')
+            else:
+                sio.emit('qvote poke', [player, 'bad'], namespace='/private')
+
+    def do_quest_vote(self, player, affirmative):
+        if self.state != "quester_vote":
+            return
+
+        if player not in self.questers:
+            sio.emit('qvote error', [player, "You aren't on the quest."], namespace='/private')
+        elif player in self.questers_voted:
+            sio.emit('qvote error', [player, "You've already voted on this quest."], namespace='/private')
+        elif player in self.teams["good"] and not affirmative:
+            sio.emit('qvote error', [player, "You can't vote to fail, you're on the good team! Voting to pass instead."], namespace='/private')
+            self.questers_voted.append(player)
+            sio.emit('qvote placed', player, namespace='/public')
+            sio.emit('qvote confirmation', [player, 'pass'], namespace='/private')
+        else:
+            self.questers_voted.append(player)
+            sio.emit('qvote placed', player, namespace='/public')
+            if not affirmative:
+                self.quest_fail_votes += 1
+                sio.emit('qvote confirmation', [player, 'fail'], namespace='/private')
+            else:
+                sio.emit('qvote confirmation', [player, 'pass'], namespace='/private')
+
+        if len(self.questers_voted) == len(self.questers):
+            self.do_quest_complete()
+
+    def do_quest_complete(self):
+        if len(self.players) >= 7 and self.quest == 3:
+            votes_to_fail = 2
+        else:
+            votes_to_fail = 1
+
+        status = self.quest_fail_votes < votes_to_fail
+        self.history.append([list(self.questers), "pass" if status else "fail", self.quest_fail_votes, self.players[self.current_quest_leader]])
+        sio.emit('qvote finish', ["pass" if status else "fail", self.quest_fail_votes], namespace='/public')
+
+        if status:
+            self.successes += 1
+        else:
+            self.failures += 1
+
+        if self.failures == 3:
+            self.do_evil_win()
+        elif self.successes == 3:
+            self.do_assassin_pick()
+        else:
+            self.next_quest()
+
+    def do_evil_win(self):
+        sio.emit('game over', 'The bad team won.', namespace='/public')
+        self.do_game_over()
+
+    def do_assassin_pick(self):
+        self.state = "assassin_pick"
+        sio.emit('pick target', self.roles['assassin'], namespace='/public')
+
+    def do_assassin_kill(self, player, target):
+        if self.state != "assassin_pick":
+            return
+
+        if player != self.roles["assassin"]:
+            sio.emit('target error', "{} isn't the assassin.".format(player), namespace='/public')
+        elif target not in self.teams["good"]:
+            sio.emit('target error', "They're not on the good team. Pick again.", namespace='/public')
+        else:
+            if self.player_roles[target] == "merlin":
+                self.do_evil_win()
+            else:
+                self.do_good_win()
+
+    def do_good_win(self):
+        sio.emit('game over', 'The good team won.', namespace='/public')
+        self.do_game_over()
+
+    def do_game_over(self):
+        global joined_players
+        sio.emit('game over', " ".join(["{} was {}.".format(*i) for i in self.player_roles.items()]), namespace='/public')
+        self.state = "no_game"
+        joined_players = []
+
+
+def authenticated_only(f):
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
+        if not request.headers.get('Authentication') == config['secret']:
+            disconnect()
+        else:
+            return f(*args, **kwargs)
+    return wrapped
+
+def game_req(errtype):
+    def wrapper(f):
+        @functools.wraps(f)
+        def wrapped(*args, **kwargs):
+            if game is None:
+                emit(errtype, "There is no game running.", broadcast=True, namespace='/public')
+                return
+            print("calling original function")
+            f(*args, **kwargs)
+        return wrapped
+    return wrapper
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@sio.on('game start request', namespace='/private')
+@authenticated_only
+def start_game(player):
+    global game, joined_players
+    if player not in joined_players:
+        emit('game start error', "You have to join the game before you can start it.", broadcast=True, namespace='/public')
+    elif len(joined_players) < 5:
+        emit('game start error', "The game must have at least five players.", broadcast=True, namespace='/public')
+    elif game is not None and game.state != "no_game":
+        emit('game start error', "The game is already ongoing.", broadcast=True, namespace='/public')
+    else:
+        emit('game start', joined_players, broadcast=True, namespace='/public')
+        game = AvalonGame(joined_players)
+        game.assign_roles()
+        game.next_quest()
+
+@sio.on('join game request', namespace='/private')
+@authenticated_only
+def join_game(player):
+    if player in joined_players:
+        emit('join game error', "You're already in the game, {}.".format(player), broadcast=True, namespace='/public')
+    elif len(joined_players) >= 10:
+        emit('join game error', "There are too many players.", broadcast=True, namespace='/public')
+    elif game is not None and game.state != "no_game":
+        emit('join game error', "Game already running.", broadcast=True, namespace='/public')
+    else:
+        joined_players.append(player)
+        emit('join game', [player, joined_players], broadcast=True, namespace='/public')
+
+@sio.on('leave game request', namespace='/private')
+@authenticated_only
+def leave_game(player):
+    if player not in joined_players:
+        emit('leave game error', "You're not playing.".format(player), broadcast=True, namespace='/public')
+    elif game is not None and game.state != "no_game":
+        emit('leave game error', "Game already started.", broadcast=True, namespace='/public')
+    else:
+        joined_players.remove(player)
+        emit('leave game', [player, joined_players], broadcast=True, namespace='/public')
+
+@sio.on('kill player request', namespace='/private')
+@authenticated_only
+@game_req("target error")
+def kill_player(args):
+    player, target = args
+    game.do_assassin_kill(player, target)
+
+
+@sio.on('propose players', namespace='/private')
+@authenticated_only
+@game_req("proposal error")
+def propose_players(args):
+    player, players = args
+    game.propose(player, players)
+
+@sio.on('vote request', namespace='/private')
+@authenticated_only
+@game_req("vote error")
+def vote(args):
+    player, truefalse = args
+    game.vote_on_questers(player, truefalse)
+
+
+@sio.on('qvote request', namespace='/private')
+@authenticated_only
+@game_req("qvote error")
+def qvote(args):
+    player, truefalse = args
+    game.do_quest_vote(player, truefalse)
+
+if __name__ == '__main__':
+    sio.run(app, host=hostname, port=portnum)
